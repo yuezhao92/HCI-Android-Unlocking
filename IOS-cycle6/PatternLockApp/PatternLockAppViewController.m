3535e74cbcf963190e27ba1715e0d15cda1a701f
Juik <fred.qiuzhongtian@gmail.com>
2016-03-17 14:04:44 -0400

    IOS circle 6

---
+++ IOS-cycle6/PatternLockApp/PatternLockAppViewController.m
@@ -0,0 +1,97 @@
+//
+//  PatternLockAppViewController.m
+//  PatternLockApp
+//
+//  Created by Purnama Santo on 11/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PatternLockAppViewController.h"
+#import "DrawPatternLockViewController.h"
+
+extern int counter;
+extern NSArray *myArray;
+
+@implementation PatternLockAppViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return NO;
+}
+
+
+- (void)lockEntered:(NSString*)key
+{
+    myArray = @[@"0102030609", @"0102030507",@"0102030507"];
+    NSLog(@"key: %@", key);
+    
+    if (![key isEqualToString:myArray[counter]]) {
+        //      [self dismissModalViewControllerAnimated:YES];
+        
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                            message:@"Wrong pattern!"
+                                                           delegate:nil
+                                                  cancelButtonTitle:nil
+                                                  otherButtonTitles:@"OK", nil];
+        [alertView show];
+        counter = counter + 1;
+    }
+    else
+    {
+        [self dismissModalViewControllerAnimated:YES];
+        counter = counter + 1;
+    }
+    
+    
+}
+
+
+- (IBAction)lockClicked:(id)sender {
+    DrawPatternLockViewController *lockVC = [[DrawPatternLockViewController alloc] init];
+    [lockVC setTarget:self withAction:@selector(lockEntered:)];
+    [self presentModalViewController:lockVC animated:YES];
+}
+
+@end
---
+++ IOS-cycle6/PatternLockApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PatternLockApp
+//
+//  Created by Purnama Santo on 11/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PatternLockAppAppDelegate.h"
+
+int counter = 0;
+NSArray  *myArray = nil;
+NSDate *methodStart = nil;
+NSDate *methodFinish = nil;
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PatternLockAppAppDelegate class]));
+    }
+}
